53e330efaca9032c13e045fc4f2e8aa0b713595e
E33aS42 <esommier@student.42.fr>
2023-12-06 11:40:17 +0100

    some clean up done in day01 , part 2 needs some extra clean up

--- Day01/day01.py
+++ Day01/day01.py
@@ -4,39 +4,31 @@ import pandas as pd
 import re
 
 input = pd.read_csv('input.txt', header = None).values
-nb = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
-arr_nb = np.empty((1000, 1), dtype=int)
+# Part 1
+
+total = 0
 i = 0
 for line in input:
 	val = ""
-	# for a in line[0]:
-	# 	if a in nb:
-	# 		val += a
-	# 		break
-	# for b in line[0][::-1]:
-	# 	if b in nb:
-	# 		val += b
-	# 		break
-	# arr_nb[i] = (int(val))
-	# i += 1
-
-
 	m_start = re.search(r"\d", line[0])
 	m_end = re.search(r"\d", line[0][::-1])
 	if m_start:
 		val += m_start.group()
 	if m_end:
 		val += m_end.group()
-	arr_nb[i] = (int(val))
+	total += int(val)
 	i += 1
 	
-print(sum(arr_nb))
+print(total)
+
+
+# Part 2
 
 dict_nb = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
 numbers = list(dict_nb.keys())
-arr_nb2 = np.empty((1000, 1), dtype=int)
+total = 0
 i = 0
 for line in input:
 	# line = ["hmbfjdfnp989mfivefiverpzrjs"]
@@ -64,15 +56,13 @@ for line in input:
 	for j in ind_str:
 		dict_str |= j
 
-	
 	keys = list(dict_str.keys())
 
 	min_keys = min(keys)
 	max_keys = max(keys)
 	val = str(dict_str[min_keys]) + str(dict_str[max_keys])
 
-	arr_nb2[i] = (int(val))
+	total += int(val)
 	i += 1
-	# break
 
-print(sum(arr_nb2))
+print(total)
